fa4e0991ed9dc7924e1b658cdee25a0ece41391a
gaurashutosh <ashutoshgond2002@gmail.com>
2025-04-14 14:01:31 +0530

    Created the business logic of user

--- backend- CareerHive/index.js
+++ backend- CareerHive/index.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
 
 
 dotenv.config({});
@@ -17,12 +18,12 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());// for storing the cookie into the browser
 
 //for testing the connection
-// app.get('/home',(req,res)=>{
-//     return res.status(200).json({
-//         message:"I am coming from the server",
-//         success : true
-//     })
-// })
+app.get('/home',(req,res)=>{
+    return res.status(200).json({
+        message:"I am coming from the server",
+        success : true
+    })
+})
 
 const corsOptions ={
     origin: "http//localhost:5137",
@@ -34,7 +35,17 @@ app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
+//-------------APIs--------------
+app.use("/api/v1/user",userRoute);
+
+
+
+
 app.listen(PORT,()=>{
     connectDB();
     console.log(`The server is running at port ${PORT}`);
 });
+
+
+// "http://localhost:8000/api/v1/user/login"
+// "http://localhost:8000/api/v1/user/profile/update"
--- backend- CareerHive/utils/db.js
+++ backend- CareerHive/utils/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("The database was connected successfully");
+        console.log("The database is connected successfully");
     } catch (error) {
         console.error("MongoDB connection failed:", error.message);
     }
